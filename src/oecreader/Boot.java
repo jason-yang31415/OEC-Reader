5fca22200e1d1a8aea2add0debf62833f3884553
jason-yang31415 <jason.yang31415@gmail.com>
2015-08-26 00:24:33 -0400

    Levenshtein Distance
    * Search algorithm with levenshtein distance, sorted by similarity

--- src/oecreader/Boot.java
+++ src/oecreader/Boot.java
@@ -19,10 +19,13 @@ import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.InputMismatchException;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 import java.util.zip.GZIPInputStream;
 
 import javax.imageio.ImageIO;
@@ -78,46 +81,124 @@ public class Boot {
 	}
 	
 	public ArrayList<Data> search(String search, boolean searchSys, boolean searchStar, boolean searchPlanet){
-		ArrayList<Data> matches = new ArrayList<Data>();
+		ArrayList<DistancePair> matches = new ArrayList<DistancePair>();
 		
 		for (StarSystem sys : systems){
-			boolean sysMatch = false;
-			
-			for (String n : sys.names){
-				if (n.toLowerCase().contains(search.toLowerCase())){
-					sysMatch = true;
+			if (searchSys){
+				ArrayList<Integer> distances = new ArrayList<Integer>();
+				for (String n : sys.names){
+					distances.add(levenshtein(search, n));
 				}
+				
+				int distance = Collections.min(distances);
+				matches.add(new DistancePair(sys, distance));
 			}
 			
-			if (sysMatch && searchSys)
-				matches.add(sys);
-			
 			for (Star star : sys.stars){
-				boolean starMatch = false;
-				
-				for (String n : star.names){
-					if (n.toLowerCase().contains(search.toLowerCase()))
-						starMatch = true;
+				if (searchStar){
+					ArrayList<Integer> distances = new ArrayList<Integer>();
+					for (String n : star.names){
+						distances.add(levenshtein(search, n));
+					}
+					
+					int distance = Collections.min(distances);
+					matches.add(new DistancePair(star, distance));
 				}
-				
-				if (starMatch && searchStar)
-					matches.add(star);
 			}
 			
 			for (Planet p : sys.planets){
-				boolean planetMatch = false;
-				
-				for (String n : p.names){
-					if (n.toLowerCase().contains(search.toLowerCase()))
-						planetMatch = true;
+				if (searchPlanet){
+					ArrayList<Integer> distances = new ArrayList<Integer>();
+					for (String n : p.names){
+						distances.add(levenshtein(search, n));
+					}
+					
+					int distance = Collections.min(distances);
+					matches.add(new DistancePair(p, distance));
 				}
-				
-				if (planetMatch && searchPlanet)
-					matches.add(p);
 			}
 		}
 		
-		return matches;
+		Collections.sort(matches, new Comparator<DistancePair>(){
+
+			@Override
+			public int compare(DistancePair d1, DistancePair d2) {
+				// TODO Auto-generated method stub
+				return d1.distance - d2.distance;
+			}
+			
+		});
+		
+		boolean exact = false;
+		for (DistancePair dp : matches){
+			Data d = dp.data;
+			for (String n : d.names){
+				if (n.equalsIgnoreCase(search))
+					exact = true;
+			}
+		}
+		
+		if (exact){
+			ArrayList<DistancePair> remove = new ArrayList<DistancePair>();
+			for (DistancePair dp : matches){
+				Data d = dp.data;
+				boolean match = false;
+				for (String n : d.names){
+					if (n.equalsIgnoreCase(search))
+						match = true;
+				}
+				if (!match)
+					remove.add(dp);
+			}
+			for (DistancePair r : remove)
+				matches.remove(r);
+		}
+		
+		ArrayList<Data> results = new ArrayList<Data>();
+		for (DistancePair dp : matches)
+			results.add(dp.data);
+		
+		return results;
+	}
+	
+	public int levenshtein(CharSequence lhs, CharSequence rhs) {                          
+	    int len0 = lhs.length() + 1;                                                     
+	    int len1 = rhs.length() + 1;                                                     
+	                                                                                    
+	    // the array of distances                                                       
+	    int[] cost = new int[len0];                                                     
+	    int[] newcost = new int[len0];                                                  
+	                                                                                    
+	    // initial cost of skipping prefix in String s0                                 
+	    for (int i = 0; i < len0; i++) cost[i] = i;                                     
+	                                                                                    
+	    // dynamically computing the array of distances                                  
+	                                                                                    
+	    // transformation cost for each letter in s1                                    
+	    for (int j = 1; j < len1; j++) {                                                
+	        // initial cost of skipping prefix in String s1                             
+	        newcost[0] = j;                                                             
+	                                                                                    
+	        // transformation cost for each letter in s0                                
+	        for(int i = 1; i < len0; i++) {                                             
+	            // matching current letters in both strings                             
+	            int match = (lhs.charAt(i - 1) == rhs.charAt(j - 1)) ? 0 : 1;             
+	                                                                                    
+	            // computing cost for each transformation                               
+	            int cost_replace = cost[i - 1] + match;                                 
+	            int cost_insert  = cost[i] + 1;                                         
+	            int cost_delete  = newcost[i - 1] + 1;                                  
+	                                                                                    
+	            // keep minimum cost                                                    
+	            newcost[i] = Math.min(Math.min(cost_insert, cost_delete), cost_replace);
+	        }                                                                           
+	                                                                                    
+	        // swap cost/newcost arrays                                                 
+	        int[] swap = cost; cost = newcost; newcost = swap;                          
+	    }                                                                               
+	                                                                                    
+	    // the distance is the cost for transforming all letters in both strings        
+	    return cost[len0 - 1];                                                          
 	}
 	
 	public void parse(){
---
+++ src/oecreader/DistancePair.java
@@ -0,0 +1,13 @@
+package oecreader;
+
+public class DistancePair {
+
+	Data data;
+	int distance;
+	
+	public DistancePair(Data data, int distance){
+		this.data = data;
+		this.distance = distance;
+	}
+	
+}
